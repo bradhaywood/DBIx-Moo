f4ec713f5203347947a9a4818fc2eaa306be7597
Brad <brad@geeksware.net>
2013-03-08 14:26:18 +0000

    Updated documentation

--- lib/DBIx/Moo.pm
+++ lib/DBIx/Moo.pm
@@ -79,6 +79,75 @@ Now you can reuse this Schema whenever you want
     # get the first row object
     say $users_rs->first;
 
+=head1 METHODS
+
+=head2 connect
+
+Initialises a connection to DBI using L<DBIx::Connector>.
+
+    my $schema = MySchema->new->connect;
+
+=head2 table
+
+If the table exists in the config, it will set it as the currently active table for that instance and return a L<DBIx::Moo::ResultSet> object.
+
+    my $users = $schema->table('users');
+
+You can even chain this onto the initial C<connect> method
+
+    my $users = $schema->new->connect->table('users');
+
+=head2 search
+
+Returns a L<DBIx::Moo::ResultSet> based on your search query. If it's expecting a scalar you'll get an object, if it's expecting an array, you'll get the results
+
+    my $users = $users->search({ status => 'active' });
+    my @users = $users->search({ status => 'active' });
+    for my $user (@users) { say $user->{name} }
+
+The first hashref are the items you want to search for, the optional second hashref are extra search options, like C<order_by> and C<rows>.
+For more information on the search syntax, please check out L<SQL::Abstract::More> as it uses this.
+
+=head2 find
+
+If you have a primary key set you can use find to search for a particular value. It will search for the value matched against the primary key.
+
+    if (my $user = $users->find(10)) {
+        say "Found user with ID 10";
+    }
+
+This will return a L<DBIx::Moo::Result> object
+
+=head2 count
+
+Simply returns the number of rows from a DBIx::Moo::ResultSet result
+
+    say "There are " . $resultset->count . " rows";
+
+=head2 first
+
+Retrieves the first row of a resultset as a DBIx::Moo::Result object
+
+    say $resultset->first->{name};
+
+=head2 last
+
+Same as first, but gets the last row instead
+
+=head2 method
+
+Injects a convenience method into a resultset
+
+    my $users = $schema->table('users');
+    $users->method('get_me_the_first_three' => sub {
+        return shift->search(undef, { rows => 3, order_by => [qw/+name/] });
+    });
+
+    my @first_three_users = $users->get_me_the_first_three;
+    for my $user (@first_three_users) {
+        ...
+    }
+
 =cut
 
 1;
